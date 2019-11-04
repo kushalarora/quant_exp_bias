09f38fb18116498eaa2d842fb2d9a5bb050a9d18
Kushal Arora <kushal18@gmail.com>
2019-11-04 16:17:39 -0500

    fixed run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,7 @@ set -eux
 
 export NUM_SAMPLES=10000
 export indir="$(date '+%Y_%m_%d_%H_%M')/"
-export FSA_GRAMMAR_STRING="q0 -> 'S' q1 [0.9900] | 'a' q1 [0.0025] | 'b' q1 [0.0025] | 'c' q1 [0.0025] | 'E' q1 [0.0025]
-				           q1 -> 'S' q1 [0.0025] | 'a' q1 [0.3000] | 'b' q1 [0.3000] | 'c' q1 [0.3000] | 'E' q1 [0.0025]
-				           q1 -> 'S' q2 [0.0025] | 'a' q2 [0.0300] | 'b' q2 [0.0300] | 'c' q2 [0.0300] | 'E' q2 [0.0025]
-				           q2 -> 'S' [0.0025] | 'a' [0.0025] | 'b' [0.0025] | 'c' [0.0025] | 'E' [0.9900]"
-
-
+export FSA_GRAMMAR_FILENAME='grammar_templates/default_grammar.txt'
 export ARTIFICIAL_GRAMMAR_TRAIN="results/artificial_grammar/${indir}/oracle_samples_train.txt"
 export ARTIFICIAL_GRAMMAR_DEV="results/artificial_grammar/${indir}/oracle_samples_dev.txt"
 
