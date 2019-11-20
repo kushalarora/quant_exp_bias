e534eefff3e13aa779dda535bded7744d0483884
hareeshbahuleyan <hareeshbahuleyan@gmail.com>
2019-11-20 17:53:51 -0500

    gpt-2 tokenizer and lm head with parallelization

--- quant_exp_bias/oracles/natural_language_oracle.py
+++ quant_exp_bias/oracles/natural_language_oracle.py
@@ -2,10 +2,11 @@ import math
 import torch
 import logging
 
-from pytorch_transformers import OpenAIGPTTokenizer, GPT2Tokenizer
-from pytorch_transformers import OpenAIGPTLMHeadModel, GPT2LMHeadModel
+from typing import List
+from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 from quant_exp_bias.oracles.oracle_base import Oracle
+from multiprocessing import Pool
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -13,22 +14,19 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 @Oracle.register('natural_lang_oracle')
 class NaturalLanguageOracle(Oracle):
 
-    def __init__(self, lm):
+    def __init__(self, parallelize=True, num_threads=128):
         super(Oracle, self).__init__()
-        assert lm in ['gpt', 'gpt2']
 
-        if lm == 'gpt':
-            # Load pre-trained model tokenizer (vocabulary)
-            self.tokenizer = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
-            # Load pre-trained model (weights)
-            self.model = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt')
+        self._parallelize = parallelize
 
-        else:
-            # Load pre-trained model (weights)
-            self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        self._num_threads = num_threads
+        self._pool = Pool(self._num_threads)
 
-            # Load pre-trained model tokenizer (vocabulary)
-            self.model = GPT2LMHeadModel.from_pretrained('gpt2')
+        # Load pre-trained model tokenizer (vocabulary)
+        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+        # Load pre-trained model (weights)
+        self.model = GPT2LMHeadModel.from_pretrained('gpt2')
 
         self.model.eval()
 
@@ -37,6 +35,10 @@ class NaturalLanguageOracle(Oracle):
         TODO: sample subset of sentences from the data used for training GPT-2
         """
         pass
+
+    def compute_sent_probs(self, sequences: List[List[str]]):
+
+        return self._pool.starmap(self.model._compute_one_sent_prob, sequences)
 
     def _compute_one_sent_prob(self, sequence):
 
