96235861cbf45c7fe33406f41c2667f72c34f4c1
Kushal Arora <kushal18@gmail.com>
2019-11-22 15:27:40 -0500

    run ptb file updated.

--- run_ptb_gpt2.sh
+++ run_ptb_gpt2.sh
@@ -1,3 +1,19 @@
 
-python -u quant_exp_bias/run.py train training_configs/natural_lang/ptb_gpt2.jsonnet -s /scratch/quant_exp_bias/results/ptb_"$(date '+%Y_%m_%d_%H_%M_%S')" --include-package quant_exp_bias
-python quant_exp_bias/run.py  quantify-exposure-bias /scratch/quant_exp_bias/results/ptb_2019_11_21_23_21_10/model.tar.gz   --include-package quant_exp_bias --cuda-device 0 --output-dir /scratch/quant_exp_bias/results/exp_bias/  --num-trials 20
+#!/bin/sh
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=30000M
+#SBATCH --mail-type=ALL
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:1
+###########################
+
+source activate quant_exp
+
+export indir="ptb_gpt2_$(date '+%Y_%m_%d_%H_%M')/"
+
+
+set -eux
+
+python -u quant_exp_bias/run.py train training_configs/natural_lang/ptb_gpt2.jsonnet -s /scratch/quant_exp_bias/results/${indir} --include-package quant_exp_bias
+python quant_exp_bias/run.py  quantify-exposure-bias ${indir}/model.tar.gz   --include-package quant_exp_bias --cuda-device 0 --output-dir /scratch/quant_exp_bias/results/exp_bias/  --num-trials 20
