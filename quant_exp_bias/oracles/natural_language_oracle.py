cfb58b3a287225e44fd31c2ee2831320e2540623
Kushal Arora <kushal18@gmail.com>
2019-12-12 19:49:01 -0500

    added batch gpt ppl computation to oracle.

--- quant_exp_bias/oracles/natural_language_oracle.py
+++ quant_exp_bias/oracles/natural_language_oracle.py
@@ -1,6 +1,7 @@
+import logging
 import math
 import torch
-import logging
+import torch.nn.functional as F
 
 from typing import List
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
@@ -16,7 +17,9 @@ class NaturalLanguageOracle(Oracle):
 
     def __init__(self, model_name="gpt2", 
                        parallelize=True,
-                       num_threads=128):
+                       num_threads=128, 
+                       cuda_device=-2,
+                       batch_size=10):
         super(Oracle, self).__init__()
         # self._parallelize = parallelize
 
@@ -29,6 +32,12 @@ class NaturalLanguageOracle(Oracle):
         # Load pre-trained model (weights)
         self.model = GPT2LMHeadModel.from_pretrained(model_name).to(torch.cuda.current_device())
 
+        self.device = None
+        if cuda_device > 0:
+            self.device = "cuda:{cuda_device}"
+        elif cuda_device == -2:
+            self.device = torch.cuda.current_device() 
+        self.batch_size = batch_size
         self.model.eval()
 
     def sample_training_set(self, num_samples: int):
@@ -40,10 +49,42 @@ class NaturalLanguageOracle(Oracle):
     def compute_sent_probs(self, sequences: List[List[str]]):
         # TODO (Kushal): Try to figure out how to do this efficiently
         # by batching the inputs.
+        max_len = max([len(sequence) for sequence in sequences])
+        ids = [self.tokenizer.convert_tokens_to_ids(sequence) + [self.tokenizer.eos_token_id] * (max_len - len(sequence)) for sequence in sequences]
+        tensor_input = torch.tensor(ids).to(torch.cuda.current_device())
+        attention_mask = (tensor_input != self.tokenizer.eos_token_id).float().to(torch.cuda.current_device())
+        
+        seq_batch_size = len(sequences)
         probs = []
-        for i, sequence in enumerate(sequences):
-            tensor_input = torch.tensor([self.tokenizer.convert_tokens_to_ids(sequence)])
+
+        for i in range(0, seq_batch_size, self.batch_size):
+            inp = tensor_input[i:i+self.batch_size] if i + self.batch_size < len(sequences) else tensor_input[i:seq_batch_size]
+            mask = attention_mask[i:i+self.batch_size] if i + self.batch_size < len(sequences) else attention_mask[i:seq_batch_size]
+            
             with torch.no_grad():
-                loss = -1 * self.model(tensor_input, labels=tensor_input)[0]
-            probs.append(math.exp(loss.item()))  # perplexity
+
+                results =  model(inp, labels=inp, attention_mask=mask)
+                logits = results[1]
+                labels = inp
+
+                shift_logits = logits[..., :-1, :].contiguous()
+                shift_labels = labels[..., 1:].contiguous()
+
+                loss_batch_seq = F.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), 
+                                                    shift_labels.view(-1),
+                                                    ignore_index = -1, reduction='none').view(self.batch_size, -1)
+
+                loss_batch_seq *=mask[:, 1:]
+                seq_sizes = mask.sum(dim=-1)
+
+                loss_batch = loss_batch_seq.sum(dim=-1)/seq_sizes
+
+                for j in range(self.batch_size):
+                    probs.append(math.exp(loss_batch[j].item()))
+
+        # for i, sequence in enumerate(sequences):
+        #     tensor_input = torch.tensor([self.tokenizer.convert_tokens_to_ids(sequence)])
+        #     with torch.no_grad():
+        #         loss = -1 * self.model(tensor_input, labels=tensor_input)[0]
+        #     probs.append(math.exp(loss.item()))  # perplexity
         return probs
