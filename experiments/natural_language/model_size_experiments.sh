f157ee7e9e3769a5395763325f73399adca7653d
Kushal Arora <kushal18@gmail.com>
2020-03-24 15:27:31 -0400

    model size experiments.sh adjusted to run each model size run seperately.

--- experiments/natural_language/model_size_experiments.sh
+++ experiments/natural_language/model_size_experiments.sh
@@ -1,7 +1,9 @@
 !#/bin/bash
 set -eux
 
-for i in 50000,4,3-00:00:00 500000,2,3-00:00:00, 2000000,1,5-00:00:00; do 
-    IFS=","; set -- $i; 
-    sbatch -t $3 -J nl_mse_$1 ./launcher_basic.sh python -u experiments/natural_language/model_size_experiments.py --num_samples $1 --num_runs $2
+for model_size in xsmall small medium large xlarge; do 
+    for i in 25000,6,10:00:00 100000,4,16:00:00, 500000,2,2-00:00:00; do 
+        IFS=","; set -- $i; 
+        sbatch -t $3 -J nl_mse_$1 ./launcher_basic.sh python -u experiments/natural_language/model_size_experiments.py --num_samples $1 --num_runs $2 --model_sizes model_size;
+    done
 done
