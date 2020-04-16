512c1213843b409af46e106cab865ee50462670f
kkhetarpal <khimyakhetarpal@gmail.com>
2020-04-15 22:29:20 -0400

    sending mails to my mila.quebec account

--- launcher_basic.sh
+++ launcher_basic.sh
@@ -4,7 +4,8 @@
 #SBATCH --account=rrg-bengioy-ad
 #SBATCH --nodes=1
 #SBATCH --mem=60000M
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=ALL,TIME_LIMIT,BEGIN,END,FAIL
+#SBATCH --mail-user=arorakus@mila.quebec
 #SBATCH --time=23:00:00
 #SBATCH --gres=gpu:1
 #SBATCH -o logs/slurm-%x-%j.out
