some cleanup in reinforce_decoder. Nothing major and fix for BLEU brevity penalty computation.

quant_exp_bias/modules/cost_functions/noise_oracle_likelihood_cost_function.py
@@ -52,11 +52,11 @@ class NoiseOracleCostFunction(CostFunction):
             gold_len, pred_len = (len(gold_labels[i]), len(predictions[i]))
             # This encourages model to generate sequences which are of equal
             # length as gold sequence.
-            brevity_penality = np.abs(1 - float(gold_len)/pred_len)  \
-                                    if pred_len > gold_len and  \
+            brevity_penality = 1 - float(gold_len)/pred_len  \
+                                    if gold_len  > pred_len  and  \
                                         self._add_brevity_penalty \
                                else 0
-            oracle_probs.append(np.log(oracle_probs_and_seq_probs[i][0] + 1e-45) - brevity_penality)
+            oracle_probs.append(np.log(oracle_probs_and_seq_probs[i][0] + 1e-45) + brevity_penality)
 
         # We return neg log prob.
         # The objective should be minimize this cost to 0.

quant_exp_bias/modules/decoders/reinforce_decoder.py
@@ -129,7 +129,6 @@ class QuantExpReinforceDecoder(QuantExpSEARNNDecoder):
                                        target_tokens: Dict[str, torch.LongTensor]):
 
         if self._combiner_mode == 'rl':
-
             # rollin_logits: (batch_size, num_rollin_steps, num_classes)
             rollin_logits = rollin_output_dict['logits'].squeeze(1)
             batch_size, num_rollin_steps, num_classes = rollin_logits.shape
@@ -159,9 +158,11 @@ class QuantExpReinforceDecoder(QuantExpSEARNNDecoder):
                 if self.training_iteration < self._num_mle_iters:
                     loss_batch = rollin_output_dict['loss_batch']
                 else:
-                    rewards = rollout_reward_batch.detach()
-                    rewards = F.softmax(rewards, dim=1) #torch.exp(rewards) # (rewards - rewards.mean())/rewards.std()
-
+                    # rewards = rollout_reward_batch.detach()
+                    # rewards = F.softmax(rewards * self._temperature, dim=1) 
+                    rewards = torch.exp(rollout_reward_batch.detach()) 
+                    # rewards = (rewards - rewards.min())/(rewards.max() - rewards.min())
+                
                     # predictions: (batch_size, rollout_steps, rollout_steps - 1)
                     predictions = rollout_output_dict["predictions"].squeeze(dim=2)
                     predictions = predictions[:, :, 1:]
@@ -190,6 +191,9 @@ class QuantExpReinforceDecoder(QuantExpSEARNNDecoder):
                     num_tokens_per_seq = log_prob_mask.sum(dim=-1)
 
                     rollout_rl_loss_batch = (summed_reward_log_probs/num_tokens_per_seq).sum(dim=-1)
+                    # if self.training_iteration % 20 == 1:
+                        # import pdb; pdb.set_trace()
+                        # for x in self._decode_tokens( predictions[2], vocab_namespace=self._target_namespace, truncate=True): print(' '.join(x))
 
                     loss_batch = self._rollin_rollout_mixing_coeff * rollin_output_dict['loss_batch'] + \
                                   (1 - self._rollin_rollout_mixing_coeff) * rollout_rl_loss_batch