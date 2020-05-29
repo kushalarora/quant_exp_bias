5003860231fdd3036217aac3769b0a45c06f84da
Kushal Arora <kushal18@gmail.com>
2020-05-29 17:14:25 -0400

    bug fix in dataset experiments.py regarding only quantify.

--- experiments/natural_language/dataset_experiments.py
+++ experiments/natural_language/dataset_experiments.py
@@ -18,7 +18,10 @@ def dataset_experiments(main_args,
                                     run=num_run,
                                     param_path=param_path,
                                     sample_from_file=True,
-                                    dataset_filename='data/wmt_news_2017/news.2017.en.shuffled.deduped.filtered')
+                                    dataset_filename='data/wmt_news_2017/news.2017.en.shuffled.deduped.filtered',
+                                    run_serialization_dir=args.run_serialization_dir,
+                                    only_quantify=args.only_quantify,
+                                    )
 
         assert len(run_metrics) == 1, \
             'For this experiment, there should only be one final metric object for a run.'
@@ -46,8 +49,6 @@ if __name__ == '__main__':
                                                 debug=args.debug,
                                                 offline=args.offline,
                                                 experiment_text=args.exp_msg,
-                                                run_serialization_dir=args.run_serialization_dir,
-                                                only_quantify=args.only_quantify,
                                             )
 
     dataset_experiments_params = [(10000, 8), (50000, 6) , (500000, 4), (2000000, 2), (5000000, 1)]
